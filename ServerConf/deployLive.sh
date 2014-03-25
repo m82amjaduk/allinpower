47c5316df7ff9740296106fcf152bc7e6b2fdd17
Amzad Mojumder <amzad.fof@gmail.com>
2014-03-25 22:47:17 +0000

    q

--- ServerConf/deployLive.sh
+++ ServerConf/deployLive.sh
@@ -1,3 +1,4 @@
 scp -P 2365 -r /home/amzad/dev/allinpower/src/web/* root@109.203.123.78:/home/acn/public_html/
 
 #deploy :: ../ServerConf/deployLive.sh
+##
--- ServerConf/gitPush.sh
+++ ServerConf/gitPush.sh
@@ -4,9 +4,15 @@ MESSAGE=$1;
 
 git status
 git add .
-git commit -m $MESSAGE
+git commit -m "$MESSAGE"
 git push
 
 
 
+
+
 git status
+
+
+
+## ~sfvjskfjs
