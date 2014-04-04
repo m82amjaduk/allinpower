8cddda3acf198c3849bf55bba7baedeecd840ed5
Amzad Mojumder <amzad.fof@gmail.com>
2014-04-04 16:32:19 +0100

    Added

---
+++ src/web/application/views/fu.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amzad
+ * Date: 04/04/14
+ * Time: 15:20
+ */
+?>
+
+<html>
+<head>
+    <title></title>
+    <meta http-equiv="refresh" content="2000; url=http://mjamjad.acnshop.eu/default.asp?CO_LA=GB_EN" />
+    <link rel="stylesheet" type="text/css" href="<?=base_url()?>cono/css/style.css" />
+</head>
+<body>
+
+
+<div class="header_resize" >
+    <div class="button">
+        <a href="<?=base_url()?>" >Home </a>
+    </div>
+    <div class="clear"></div>
+
+    <div class="button">
+        <a href="<?=base_url()?>cono/docs/TransferForm.pdf" >Transfer Form </a>
+    </div>
+    <div class="clear"></div>
+
+    <div class="button">
+        <a href="http://first-utility.force.com/signup">Apply For License</a>
+    </div>
+    <div class="clear"></div>
+
+    <div class="button">
+        <a href="http://firstutility.mindflash.com"> First Utility Certificate </a>
+    </div>
+    <div class="clear"></div>
+        <br /> <br />
+    Help Desk / Send your Passport: sales.admin@first-utility.com
+</div>
+
+
+
+</body>
+</html>
--- src/web/application/views/welcome_message.php
+++ src/web/application/views/welcome_message.php
@@ -1,57 +1,81 @@
 <!DOCTYPE html>
 
-<style>
-
-    html {
-        background: url(http://www.energycentric.co.uk/wp-content/uploads/2010/08/electric.jpg) no-repeat center center fixed;
-        -webkit-background-size: cover;
-        -moz-background-size: cover;
-        -o-background-size: cover;
-        background-size: cover;
-    }
-    .header_resize {
-        margin: 0 auto;
-        padding: 10px 70px 10px 70px;
-        width: 370px;
-        background: gray;
-    }
-</style>
+<!--<style>-->
+<!---->
+<!--    html {-->
+<!--        background: url(http://www.energycentric.co.uk/wp-content/uploads/2010/08/electric.jpg) no-repeat center center fixed;-->
+<!--        -webkit-background-size: cover;-->
+<!--        -moz-background-size: cover;-->
+<!--        -o-background-size: cover;-->
+<!--        background-size: cover;-->
+<!--    }-->
+<!--    .header_resize {-->
+<!--        margin: 0 auto;-->
+<!--        padding: 10px 70px 10px 70px;-->
+<!--        width: 370px;-->
+<!--        background: gray;-->
+<!--    }-->
+<!--</style>-->
 
 <html>
 <head>
     <title></title>
     <meta http-equiv="refresh" content="2000; url=http://mjamjad.acnshop.eu/default.asp?CO_LA=GB_EN" />
+    <link rel="stylesheet" type="text/css" href="<?=base_url()?>cono/css/style.css" />
 </head>
 <body>
 
 
-<div class="header_resize" >
-    <a href="/"> <img src="http://allinpower.co.uk/img/logo.png" width="100%" /> </a>
-    <a href="http://mjamjad.acnshop.eu/default.asp?CO_LA=GB_EN"> ACN Shop </a><br /><br />
+<div class="header_resize_1" >
+    <a href="/" > <img src="http://allinpower.co.uk/img/logo.png" width="100%" /> </a>
 
-    <a href="https://myacn.acninc.com/MyACN/homePage.do">Admin</a><br /><br />
+    <div class="button">
+        <a href="http://mjamjad.acnshop.eu/default.asp?CO_LA=GB_EN" > ACN Shop </a>
+    </div>
+    <div class="clear"> </div>
 
-    <a href="http://acneuro.co.uk">Online Registration</a> <br /> <br />
-    <a href="http://first-utility.force.com/signup">Apply For License</a> <br />
+    <div class="button">
+        <a href="https://myacn.acninc.com/MyACN/homePage.do">Admin</a>
+    </div>
+    <div class="clear"> </div>
 
+    <div class="button">
+        <a href="http://acneuro.co.uk">Online Registration</a>
+    </div>
+    <div class="clear"> </div>
 
-    <a href="http://firstutility.mindflash.com"> Gass ELE Certificate </a> <br /> <br />
+    <div class="button">
+        <a href="https://myacn.acninc.com/MyACN/RAPDFDownload.do">Manual registation Form</a>
+    </div>
 
+    <div class="clear"> </div>
 
-    <a href="https://myacn.acninc.com/RetrievePwd/enterRetrivePwd.do?ctry_lang=GB_EN">First Time Login</a><br />
+    <div class="button">
+        <a href="/videos"> BusinessPresentation </a>
+    </div>
+    <div class="clear"> </div>
 
+    <div class="button">
+        <a href="/fu">First Utility Tools</a>
+    </div>
+    <div class="clear"> </div>
 
-    <a href="/videos/"> BusinessPresentation </a>  <br />
+    <div class="button">
+        <a href="/vm">Virgin Media Tools</a>
+    </div>
+    <div class="clear"> </div>
 
-    sales.admin@first-utility.com  <br />
+    <div class="button">
+        <a href="/dp">Digital Phone Tools</a>
+    </div>
+
+    <br /><br /><br /> <br />
     uk_helpdesk@acneuro.com <br />
     020 3608 5052 <br />
 
 
-    <a href="https://myacn.acninc.com/MyACN/RAPDFDownload.do">Manual registation Form </a> <br / >
 
-    <a href="https://www.youtube.com/watch?v=Ye98lZGwTJU">Business Oppertunity </a>  <br />
-
+    <!--a href="https://myacn.acninc.com/RetrievePwd/enterRetrivePwd.do?ctry_lang=GB_EN">First Time Login</a><br /-->
 </div>
 
 
